47d3465d46b80c8964a0d96c5cc6bd6847427cea
GreenNope <tlash1000@gmail.com>
2019-08-29 13:39:25 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,2 +1,19 @@
 var canvas = document.getElementById("GameCanvas");
 var ctx = canvas.getContext("2d");
+
+var map = [
+  [1, 0],
+  [0, 1]
+  ]
+
+function render(contextIn, mapIn) {
+  for (var y = 0; y < mapIn.length; y++) {
+   for (var x = 0; x < mapIn.length; x++) {
+     if (mapIn[y][x] == "1") {
+       contextIn.fillStyle = "#00FF00"
+     } else {contextIn.fillStyle = "#000000"}
+     contextIn.fillRect(x * 20, y * 20, (x + 1) * 20, (y + 1) * 20); 
+   }
+ }
+  
+}
